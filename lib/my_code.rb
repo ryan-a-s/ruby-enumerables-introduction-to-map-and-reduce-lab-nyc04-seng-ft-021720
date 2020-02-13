a17c05d2c0c446673274695b6cfeac5bd8bde358
Ryan Seit <ryan@ryanseit.com>
2020-02-13 00:56:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,68 @@
 # My Code here....
+# map-like methods
+def map_to_negativize(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length
+    new_array << (source_array[i] * -1)
+    i += 1
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  i = 0
+  new_array = Array.new
+  while i < source_array.length
+    new_array << source_array[i]
+    i += 1
+  end
+  new_array
+end
+
+def map_to_double(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length
+    new_array << (source_array[i] * 2)
+    i += 1
+  end
+  new_array
+end
+
+def map_to_square(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length
+    new_array << (source_array[i] ** 2)
+    i += 1
+  end
+  new_array
+end
+
+# reduce-like methods
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0
+  total = starting_point
+  while i < source_array.length
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  if !source_array.include?(false)
+    return true
+  else
+    return false
+  end
+end
+
+def reduce_to_any_true(source_array)
+  if source_array.any? == true
+    return true
+  else
+    return false
+  end
+end
