2959e085cefc0e41087c74c35e24a46b53ccba33
Ryan Seit <ryan@ryanseit.com>
2020-02-13 00:31:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,69 @@
 # My Code here....
+# map-like methods
+def map_to_negativize(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length
+    new_array << (source_array[i] * -1)
+    i += 1
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  i = 0
+  new_array = Array.new
+  while i < source_array.length
+    new_array << source_array[i]
+    i += 1
+  end
+  new_array
+end
+
+def map_to_double(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length
+    new_array << (source_array[i] * 2)
+    i += 1
+  end
+  new_array
+end
+
+def map_to_square(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length
+    new_array << (source_array[i] ** 2)
+    i += 1
+  end
+  new_array
+end
+
+# reduce-like methods
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0
+  total = starting_point
+  while i < source_array.length
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  value =
+  while i < source_array.length
+    if source_array[i]
+      value = true
+    else
+      value = false
+    i += 1
+    end
+  end
+  value
+end
+
+def reduce_to_any_true(source_array)
+end
