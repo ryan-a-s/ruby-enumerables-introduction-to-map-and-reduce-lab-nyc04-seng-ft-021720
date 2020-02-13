d3170b8238579c6b78e04d76b7dfad35d35b4f03
Ryan Seit <ryan@ryanseit.com>
2020-02-13 00:37:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,17 +52,11 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0
-  value =
-  while i < source_array.length
-    if source_array[i]
-      value = true
-    else
-      value = false
-    i += 1
-    end
+  if !source_array.include?(false)
+    return true
+  else
+    return false
   end
-  value
 end
 
 def reduce_to_any_true(source_array)
